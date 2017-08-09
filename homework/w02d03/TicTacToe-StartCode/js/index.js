fc14e69ad92a839bbaeafaa6426a15246bcdbf27
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-08-09 23:58:18 +0100

    updated html, better win logic; playerarray inner loop

--- homework/w02d03/TicTacToe-StartCode/js/index.js
+++ homework/w02d03/TicTacToe-StartCode/js/index.js
@@ -3,11 +3,10 @@ player1moves = [];
 player2moves = [];
 playerturn = 0;
 winCombinations = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
-scoreCount = 0;
+matchFound = 0;
 // 
 // -------------------------------------------
 // gamePlay
-function gamePlay(){
 //**// playerturn set to true
 // //  player1move
 // //		-player1 clicks on box
@@ -24,13 +23,11 @@ function gamePlay(){
 //		player2 turn
 //		player2moves
 //		make comparison, until win
-}
 // grab box, return attribute
-function getBox(){
+function gamePlay(){
 	var clickedBox;
 	var $boxes = $('td');
 	playerturn = 0;
-
 
 	$boxes.each(function(index, box){
 		$(box).on("click",function(){
@@ -38,70 +35,77 @@ function getBox(){
 			if (playerturn%2 == 0){
 				player1moves.push(clickedBox);
 				console.log('player1moves arr',player1moves)
-				$(box).addClass('X');
-				$(box).html('X');
+				$(box).addClass('X').html('X');
+				$('.playerTurn').html("It is O's turn");
 			} else {
 				player2moves.push(clickedBox);
 				console.log('player2moves arr',player2moves)
-				$(box).addClass('O');
-				$(box).html('O');
+				$(box).addClass('O').html('O');
+				$('.playerTurn').html("It is X's turn");
 			}
 			playerturn++;
-			if (playerturn > 5){
+			if (playerturn >= 5){
 				//start comparisons
-				console.log('i want to compare now')
-				compare(player1moves, player2moves);
+				if (playerturn%2 == 0){
+					compare(player1moves);
+				} else {
+					compare(player2moves);
+				}
 			}
 		})
 
 	});
 
-	
+	$('#reset').on('click', function(){
+		//clear variables
+		player1moves = [];
+		player2moves = [];
+		playerturn = 0;
+		matchFound = 0;
+		//clear display
+		$boxes.each(function(index, box){
+			$(box).removeClass('X').html('');
+			$(box).removeClass('O').html('');
+		})
+	})
 	
 	
 }
-getBox();
+gamePlay();
 
-// function colorBox(boxes[box]){
-// 	boxes[box].addClassName('X');
-// }
 // -------------------------------------------
 // COMPARISON
 // compare elements in player1moves array <=== .each
 // with array inside winCombinations array
 //	if array[0] included in w.c.a[index]w.c[index]
-// 		scoreCount++;
-//		if scoreCount === 3;
+// 		matchFound++;
+//		if matchFound === 3;
 //			win;
 //		else
 //			continue loop
-//			clear scoreCount after every subarray
+//			clear matchFound after every subarray
 
-function compare(player1array, player2array){
+function compare(playerArray){
 	//compare each element in playerarray
-	for(var i = 0; i < player1array.length; i++){
 		// with each subarray inside wincombinations array
-		//console.log('inside playerarray')
-		//console.log('scoreCount',scoreCount)
-		//console.log('out wc', winCombinations.length)
-		for(var subj = 0; subj < winCombinations.length; subj++){
-			//console.log('out wc', winCombinations.length)
-			//console.log('inside winCombinations subarray')
-			//console.log('player1array',[i], 'winCombinations',[subj])
-			console.log('wincom subj', winCombinations[subj])
-			for (var j = 0; j < winCombinations[subj].length; j++){
-				var abc = winCombinations[subj];
-				//console.log('subj', abc[j])
-				if (player1array[i] == abc[j]){
-					scoreCount+= 1;
-					console.log(scoreCount)
-					if (scoreCount === 3) console.log("win");
+	$(winCombinations).each(function(i, combo){
+		$(combo).each(function(j, gridPos){
+			$(playerArray).each(function(index, element){
+				if (element == gridPos){
+					matchFound++;
+					console.log(matchFound)
+					if (matchFound === 3) {
+						$('.playerTurn').html("WIN");
+						// stop loop
+						return false;
+					}
 				}
-			}
-		} 
-		scoreCount = 0;
-	}
-
+			})
+		})
+		//reset matchFound after each combination
+		matchFound = 0;
+	})
 
 
 }
+
