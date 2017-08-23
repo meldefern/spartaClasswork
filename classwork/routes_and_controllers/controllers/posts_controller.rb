99a7a87b4c0dd3406d99d79aa9b954b3aa1c45e9
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-08-23 18:10:43 +0100

    routes and controllers classwork

---
+++ classwork/routes_and_controllers/controllers/posts_controller.rb
@@ -0,0 +1,91 @@
+class PostsController < Sinatra::Base
+
+	$posts = [{
+			id: 0,
+			title: 'Post 1',
+			content: 'This is the first post'
+		},
+		{
+			id: 1,
+			title: 'Post 2',
+			content: 'This is the second post'
+		},
+		{
+			id: 2,
+			title: 'Post 3',
+			content: 'This is the third post'
+		}]
+
+	# sets root as the parent-directory of the current file
+	set :root, File.join(File.dirname(__FILE__), '..')
+
+	# sets the view directory correctly 
+	set :view, Proc.new { File.join(root, 'views') }
+
+	configure :development do
+		register Sinatra::Reloader
+	end
+
+	get '/' do
+
+		@title = "Blog Posts - - - - - -"
+
+		@posts = $posts
+
+
+		erb :'posts/index'
+
+	end
+
+	get '/new' do
+		
+		erb :'posts/new'
+
+	end
+
+	get '/:id' do
+
+		id = params[:id].to_i
+
+		@post = $posts[id]
+
+		erb :'posts/show'
+
+	end
+
+	post '/' do
+
+		id = $posts.length
+
+		new_post = {
+			id: id,
+			title: params[:title],
+			content: params[:content]
+		}
+
+		$posts.push new_post
+
+		redirect "/"
+
+	end
+
+	put '/:id' do
+
+		"UPDATE #{params[:id]} "
+
+	end
+
+	delete '/:id' do
+
+		"DELETE #{params[:id]}"
+
+	end
+
+	get '/:id/edit' do
+
+		"EDIT: #{params[:id]}"
+
+		erb :'posts/edit'
+
+	end
+end
