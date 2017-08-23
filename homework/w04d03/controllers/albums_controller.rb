1c95adb09b40b046614da20b083594ecb8ef418b
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-08-23 18:25:39 +0100

    setup generic object array

--- homework/w04d03/controllers/albums_controller.rb
+++ homework/w04d03/controllers/albums_controller.rb
@@ -1,5 +1,21 @@
 class AlbumsController < Sinatra::Base 
 
+	$albums = [{
+			id: 0,
+			title: 'Album 1',
+			release: 'date 01'
+		},
+		{
+			id:1,
+			title: 'Album 2',
+			release: 'date 02'
+		},
+		{
+			id:2,
+			title: 'Album 3',
+			release: 'date 03'
+		}]
+
 	# sets root as the parent-directory of the current file
 	set :root, File.join(File.dirname(__FILE__), '..')
 
@@ -37,6 +53,5 @@ class AlbumsController < Sinatra::Base
 	delete '/:id' do
 		"DESTROY"
 	end
-
 
 end
